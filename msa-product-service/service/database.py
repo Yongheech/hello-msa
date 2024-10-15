7c27f5d3d2ee18ac39732311c1e0a9fb57369162
alpacatnt@naver.com <alpacatnt@naver.com>
2024-10-15 15:35:23 +0900

    product service - sqlite orm 설정, 상품 등록 처리

---
+++ msa-product-service/service/database.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from models.product import Base
+
+db_url = 'sqlite:///products.db'
+
+engine = sqlalchemy.create_engine(db_url, echo=True)
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False, bind=engine)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    with SessionLocal() as db:
+        yield db
