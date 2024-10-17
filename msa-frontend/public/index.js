a538937e8e16dc91cd09bd6f350e2903f2870530
alpacatnt@naver.com <alpacatnt@naver.com>
2024-10-17 16:35:11 +0900

    frontend - 로그인한 경우에 보이는 페이지 추가, 로그아웃 기능 구현,

--- msa-frontend/public/index.js
+++ msa-frontend/public/index.js
@@ -31,6 +31,15 @@ router.get('/loginuser', function(req, res, next) {
   res.sendFile(__dirname + '/views/userlogin.html')
 });
 
+/* secure page access  */
+router.get('/secure', function(req, res, next) {
+  res.sendFile(__dirname + '/views/secure.html')
+});
+
+/* logout - session remove  */
+router.get('/logout', function(req, res, next) {
+});
+
 /* product registration */
 router.get('/product', function(req, res, next) {
   res.sendFile(__dirname + '/views/product.html')
